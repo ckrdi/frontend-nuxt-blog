f99f2524b876285a494fb4de243475a66f437e66
ckrdi <agung.cokorda@gmail.com>
2021-07-08 20:39:41 +0800

    Contact post logic and meta tag

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,44 +1,122 @@
 <template>
   <div class="section">
-    <div class="field">
-      <label class="label">Name</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Your name" />
+    <form @submit="handleSubmit">
+      <div class="field">
+        <label class="label" for="name">Name</label>
+        <div class="control">
+          <input
+            class="input"
+            name="name"
+            type="text"
+            placeholder="Your name"
+            v-model="newMessage.name"
+            required
+          />
+        </div>
       </div>
-    </div>
 
-    <div class="field">
-      <label class="label">Email</label>
-      <div class="control">
-        <input class="input" type="email" placeholder="Your email" />
+      <div class="field">
+        <label class="label" for="email">Email</label>
+        <div class="control">
+          <input
+            class="input"
+            name="email"
+            type="email"
+            placeholder="Your email"
+            v-model="newMessage.email"
+            required
+          />
+        </div>
       </div>
-    </div>
 
-    <div class="field">
-      <label class="label">Subject</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="Subject" />
+      <div class="field">
+        <label class="label" for="subject">Subject</label>
+        <div class="control">
+          <input
+            class="input"
+            name="subject"
+            type="text"
+            placeholder="Subject"
+            v-model="newMessage.subject"
+            required
+          />
+        </div>
       </div>
-    </div>
 
-    <div class="field">
-      <label class="label">Message</label>
-      <div class="control">
-        <textarea class="textarea" placeholder="Your message"></textarea>
+      <div class="field">
+        <label class="label" for="message">Message</label>
+        <div class="control">
+          <textarea
+            class="textarea"
+            name="message"
+            placeholder="Your message"
+            v-model="newMessage.message"
+            required
+          ></textarea>
+        </div>
       </div>
-    </div>
 
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-rounded is-link is-inverted">Submit</button>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-rounded is-link is-inverted">Submit</button>
+        </div>
+        <div class="control">
+          <button
+            class="button is-rounded is-link is-inverted"
+            @click="handleDeleteMessage"
+          >
+            Cancel
+          </button>
+        </div>
       </div>
-      <div class="control">
-        <button class="button is-rounded is-link is-inverted">Cancel</button>
+      <div v-if="error">
+        {{ error }}
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      newMessage: {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      },
+      error: null,
+    }
+  },
+  methods: {
+    handleDeleteMessage: function () {
+      this.newMessage.name = ''
+      this.newMessage.email = ''
+      this.newMessage.subject = ''
+      this.newMessage.message = ''
+    },
+
+    handleSubmit: async function (e) {
+      this.error = null
+      e.preventDefault()
+
+      try {
+        const response = await axios.post(
+          'https://desolate-mountain-91022.herokuapp.com/messages',
+          this.newMessage
+        )
+        this.newMessage.name = ''
+        this.newMessage.email = ''
+        this.newMessage.subject = ''
+        this.newMessage.message = ''
+        // console.log(response)
+      } catch (error) {
+        this.error = error
+      }
+    },
+  },
+}
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,19 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: "Cokorda Agung's Website",
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Website built by Cokorda Agung, a self-taught front-end developer',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
